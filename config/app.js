f5526b0b6b69fd5ae964e896ca9f8aed6fad6845
Lucas Andrade <lucas.andrade.oliveira@hotmail.com>
2016-11-02 08:44:25 -0200

    adding user authentication route

--- config/app.js
+++ config/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    secret: process.env.SECRET || 'iyqetqtre',
+    secret: process.env.SECRET || '29384ghw6asdftyiasdf675',
     database: {
         url: process.env.DB_URL || 'localhost',
         port: process.env.DB_PORT || '27017',
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,27 @@
 var express = require('express');
 var router = express.Router();
 var response = require('../config/response');
-var User = require('../models/User');
+var User = require('../models/user');
+var authenticateUser = require('../functions/authenticateUser');
+var generateUserToken = require('../functions/generateUserToken');
 
 router.get('/', function (req, res, next) {
     res.send(response.setAsSuccess().setData(req.query).get());
 });
 
 router.post('/auth', function (req, res, next) {
-    
+    var body = req.body;
+
+    if (body.password && body.username) {
+        authenticateUser(body.username, body.password, function authenticateUserSuccess(user) {
+            var token = generateUserToken(user);
+            res.send(response.setAsSuccess().setData(token).get());
+        }, function authenticateUserError(err) {
+            res.send(response.setAsFail().setMessage(err).get());
+        });
+    } else {
+        res.send(response.setAsFail().setMessage('Dados inválidos').get());
+    }
 });
 
 router.get('/list', function (req, res, next) {
