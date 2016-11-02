824f8b8edffc9dd9923792618ad4ed76c96ed42b
Lucas Andrade <lucas.andrade.oliveira@hotmail.com>
2016-11-02 09:52:17 -0200

    adding create table function and testing

---
+++ functions/createTable.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Table = require('../models/table');
+
+function createTable(data, success, fail) {
+    Table.create(data, function tableCreatedResponse(err, table) {
+        if (err) {
+            if (err.errors) {
+                var messages = {};
+                for (var field in err.errors) {
+                    if (err.errors[field].message) {
+                        messages[field] = err.errors[field].message;
+                    }
+                }
+                if (Object.keys(messages).length > 0) {
+                    return fail(messages);
+                }
+            }
+
+            return fail(err);
+        } else {
+            success(table);
+        }
+    });
+}
+
+module.exports = createTable;
---
+++ tests/functions/createTable.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var utils = require('../utils');
+var createTable = require('../../functions/createTable');
+var assert = require('assert');
+var data = {
+    name: 'Mesa 1'
+}
+
+describe('createTable: functions', function () {
+    it('should get table validation errors', function (done) {
+        createTable({}, function (table) {
+            assert.ok(false);
+            done();
+        }, function (err) {
+            assert.equal(err['name'], 'Nome da mesa é obrigatório');
+            done();
+        });
+    });
+
+    it('should create table', function (done) {
+        createTable(data, function (table) {
+            assert.ok(table);
+            assert.equal(table.name, data.name);
+            done();
+        }, function (err) {
+            assert.ok(false);
+            done();
+        });
+    });
+});
